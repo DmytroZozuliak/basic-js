95e01017f121401ae6167e51ff8a2962e88adf5a
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2021-09-10 12:41:46 +0300

    feat: solve deeper and repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,62 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  if (options === undefined) {
+    return str;
+  }
+  if (options.addition !== undefined) {
+    //  console.log(options.addition);
+    options.addition = String(options.addition);
+    //  console.log(options.addition);
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+
+  let main = [];
+  let aditional = [];
+  //   console.log(options.addition);
+  if (options.addition) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      // console.log(options.addition);
+      aditional.push(options.addition);
+    }
+    if (options.additionSeparator) {
+      aditional = aditional.join(options.additionSeparator);
+    } else {
+      aditional = aditional.join('|');
+    }
+  }
+
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      main.push(str + aditional);
+    }
+    if (options.separator) {
+      main = main.join(options.separator);
+    } else {
+      main = main.join('+');
+    }
+  }
+  //   console.log(aditional);
+  //   console.log(main);
+  return main;
 }
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,8 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let counter = 1;
+    for (const elem of arr) {
+      if (Array.isArray(elem)) {
+        counter += this.calculateDepth(arr.flat());
+        break;
+      }
+    }
+
+    return counter;
   }
 }
